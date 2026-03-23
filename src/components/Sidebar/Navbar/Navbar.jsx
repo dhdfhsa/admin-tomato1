9747ad665b224648fef628c1198e8f9c50b41d4a
dhdfhsa <safwanwr111@gmail.com>
2026-03-23 23:54:00 +0600

    8

--- src/components/Sidebar/Navbar/Navbar.jsx
+++ src/components/Sidebar/Navbar/Navbar.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
 import './Navbar.css'
-// import { assets } from '../../../assets/assets.js'; 
-// // OR if it's an image
-// import assets from '../../../assets/logo.png';
+import logo from '../../../assets/logo.png';
+import icon from '../../../assets/icon.svg';
 const Navbar = () => {
   return (
     <div className='navbar'>
       <div className="navbar-left">
         <div className="navbar-brand">
-          <img className='logo' src={assets.logo} alt="Tomato" />
+          <img className='logo' src={logo} alt="Tomato" />
           <span className="brand-text">Tomato Admin</span>
         </div>
         <div className="navbar-status">Live</div>
       </div>
 
       <div className="navbar-actions">
-        <img className='profile' src={assets.profile_image} alt="Profile" />
+        <img className='profile' src={profile_image} alt="Profile" />
       </div>
     </div>
   )
