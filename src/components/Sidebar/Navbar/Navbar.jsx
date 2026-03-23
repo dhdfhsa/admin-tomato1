f51577cf87cf95c4efc1cdce349c62cf26ef8ad2
dhdfhsa <safwanwr111@gmail.com>
2026-03-24 00:01:41 +0600

    11

--- src/components/Sidebar/Navbar/Navbar.jsx
+++ src/components/Sidebar/Navbar/Navbar.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import './Navbar.css'
-import { assets } from './../../../../../frontened/src/assets/assets';
+import { assets } from './../../../assets/assets';
 const Navbar = () => {
   return (
     <div className='navbar'>
       <div className="navbar-left">
         <div className="navbar-brand">
-          <img className='logo' src={logo} alt="Tomato" />
+          <img className='logo' src={assets.logo} alt="Tomato" />
           <span className="brand-text">Tomato Admin</span>
         </div>
         <div className="navbar-status">Live</div>
       </div>
 
       <div className="navbar-actions">
-        <img className='profile' src={icon} alt="Profile" />
+        <img className='profile' src={assets.profile_icon} alt="Profile" />
       </div>
     </div>
   )
